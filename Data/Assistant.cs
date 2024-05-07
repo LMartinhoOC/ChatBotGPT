d3a3ba4dccdf3a0163e62f2b3690373fc0d17ce6
luiz.martinho <luiz.martinho@grupoccaa.com.br>
2024-05-06 22:10:34 -0300

    Ajustes no assistant

--- Data/Assistant.cs
+++ Data/Assistant.cs
@@ -14,7 +14,7 @@ namespace ChatGPTChatBot.Data
 
         public async Task<Message> EnviaMensagem(Message mensagem)
         {
-            OpenAIAPI api = new OpenAIAPI(bearer);
+            OpenAIAPI api = new(bearer);
 
             var chat = api.Chat.CreateConversation();
             chat.Model = Model.GPT4_Turbo;
@@ -24,29 +24,43 @@ namespace ChatGPTChatBot.Data
             chat.AppendSystemMessage("Você responde tudo como se fosse o chewbacca");
 
             // give a few examples as user and assistant
-            chat.AppendUserInput("Estou tossindo muito e morrendo de frio, será que estou gripado?");
+            chat.AppendUserInput("Estou tossindo muito e morrendo de frio.");
             chat.AppendExampleChatbotOutput("É possível... Têm sentido mais algum sintoma? Precisamos investigar mais.");
 
             chat.AppendUserInput(mensagem.Content);
             var response = await chat.GetResponseFromChatbotAsync();
 
-            Message resposta = new Message();
+            Message resposta = new();
             {
-                resposta.Id = chat.MostRecentApiResult.Id;
-                resposta.Created = chat.MostRecentApiResult.Created.Value;
-                resposta.Content = response;
-                resposta.Total_tokens = chat.MostRecentApiResult.Usage.TotalTokens;
-                resposta.Autor = null;
+                resposta.Id             = chat.MostRecentApiResult.Id;
+                resposta.Created        = chat.MostRecentApiResult.Created.Value;
+                resposta.Content        = response;
+                resposta.Total_tokens   = chat.MostRecentApiResult.Usage.TotalTokens;
+                resposta.Autor          = null;
             }
 
-            return resposta;
-
-            //await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
-            //{
-            //    return mensagem;
-            //}
+            return resposta;            
         }
 
+        public async Task<Message> StreamMensagens(Message mensagem)
+        {
+            OpenAIAPI api = new(bearer);
+            var chat = api.Chat.CreateConversation();
+            chat.AppendUserInput(mensagem.Content);
 
+            await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
+            { 
+                Console.WriteLine(res);
+            }
+
+            return null;
+        }
+
+        public async void FineTuning()
+        {
+            OpenAIAPI api = new(bearer);
+
+            api.Files.GetFilesAsync();
+        }
     }
 }
