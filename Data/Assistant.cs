81766fc383ab5d4bd5466de911b4551f4e2ecf1c
Luiz Martinho <luiz.martinho@grupoccaa.com.br>
2024-05-21 06:34:30 -0300

    Implementação do histórico do chat

--- Data/Assistant.cs
+++ Data/Assistant.cs
@@ -11,56 +11,46 @@ namespace ChatGPTChatBot.Data
     public class Assistant
     {
         public string bearer { get; set; }
+        private string filepath = "../Treinamento/teste.json";
 
-        public async Task<Message> EnviaMensagem(Message mensagem)
+        public async Task<Message> EnviaMensagem(Message mensagem, List<Message> contexto)
         {
             OpenAIAPI api = new(bearer);
 
             var chat = api.Chat.CreateConversation();
-            chat.Model = Model.GPT4_Turbo;
-            chat.RequestParameters.Temperature = 0;
+
+            foreach (var message in contexto)
+            {
+                if (message.Autor == null)
+                {
+                    chat.AppendSystemMessage(message.Content);
+                }
+                else
+                {
+                    chat.AppendUserInput(message.Content);
+                }
+            }
+            chat.Model = Model.ChatGPTTurbo; //define o modelo do ChatGPT
 
             //Passa instruções ao bot
-            chat.AppendSystemMessage("Você responde tudo como se fosse o chewbacca");
-
-            // give a few examples as user and assistant
-            chat.AppendUserInput("Estou tossindo muito e morrendo de frio.");
-            chat.AppendExampleChatbotOutput("É possível... Têm sentido mais algum sintoma? Precisamos investigar mais.");
+            chat.RequestParameters.Temperature = 0;
+            chat.AppendSystemMessage("Você é um assistente médico que trabalha em um consultório médico. Sua missão é questionar o usuário e gerar um relatório breve sobre a condição dele. Como uma versão breve de um diagnóstico. Não diga ao usuário para procurar um médico, pois ele já está em um consultório.");
+            chat.AppendUserInput("Tenho tido tosse e espirros.");
+            chat.AppendExampleChatbotOutput("Parece ser um resfriado, talvez, no máximo uma gripe.");
 
             chat.AppendUserInput(mensagem.Content);
             var response = await chat.GetResponseFromChatbotAsync();
 
             Message resposta = new();
             {
-                resposta.Id             = chat.MostRecentApiResult.Id;
-                resposta.Created        = chat.MostRecentApiResult.Created.Value;
-                resposta.Content        = response;
-                resposta.Total_tokens   = chat.MostRecentApiResult.Usage.TotalTokens;
-                resposta.Autor          = null;
+                resposta.Id = chat.MostRecentApiResult.Id;
+                resposta.Created = chat.MostRecentApiResult.Created.Value;
+                resposta.Content = response;
+                resposta.Total_tokens = chat.MostRecentApiResult.Usage.TotalTokens;
+                resposta.Autor = null;
             }
 
-            return resposta;            
-        }
-
-        public async Task<Message> StreamMensagens(Message mensagem)
-        {
-            OpenAIAPI api = new(bearer);
-            var chat = api.Chat.CreateConversation();
-            chat.AppendUserInput(mensagem.Content);
-
-            await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
-            { 
-                Console.WriteLine(res);
-            }
-
-            return null;
-        }
-
-        public async void FineTuning()
-        {
-            OpenAIAPI api = new(bearer);
-
-            api.Files.GetFilesAsync();
+            return resposta;
         }
     }
 }
